e1ab725186e656abf914b2da896d10615ee7d94c
Diego PB <diego.penicaud@gmail.com>
2023-12-15 11:02:37 +0100

    maj

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ window.addEventListener('scroll', function() {
         document.body.style.backgroundColor = couleurdebase; 
     }
 });
+
+// Fonction pour rediriger si la taille de la fenêtre est inférieure à 900 pixels
+function redirigerSiPetiteFenetre() {
+    if (window.innerWidth < 900) {
+        window.location.href = 'tel.html'; // Remplacez 'page.html' par le chemin de votre page de redirection
+    }
+}
+
+// Attacher la fonction au chargement de la page et au redimensionnement de la fenêtre
+window.onload = redirigerSiPetiteFenetre;
+window.onresize = redirigerSiPetiteFenetre;
